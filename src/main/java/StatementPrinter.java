833014c9ca4bb646efa626568891b81dad7d7271
(en) <kezhkia@protonmail.com>
2023-10-06 14:57:59 +0200

    Remove useless /100 in tests.

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -2,7 +2,7 @@ import java.text.NumberFormat;
 import java.util.*;
 
 public class StatementPrinter {
-
+//TODO: .ftl freemarker for HTML
   public String print(Invoice invoice, HashMap<String, Play> plays) {
     int totalAmount = 0;
     int volumeCredits = 0;
@@ -12,21 +12,21 @@ public class StatementPrinter {
 
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-      int thisAmount = 0;
+      int priceToPay = 0;
 
       switch (play.type) {
         case "tragedy":
-          thisAmount = 40000;
+          priceToPay = 400;
           if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
+            priceToPay += 10 * (perf.audience - 30);
           }
           break;
         case "comedy":
-          thisAmount = 30000;
+          priceToPay = 300;
           if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
+            priceToPay += 100 + 5 * (perf.audience - 20);
           }
-          thisAmount += 300 * perf.audience;
+          priceToPay += 3 * perf.audience;
           break;
         default:
           throw new Error("unknown type: ${play.type}");
@@ -38,10 +38,10 @@ public class StatementPrinter {
       if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
       // print line for this order
-      result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
-      totalAmount += thisAmount;
+      result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(priceToPay), perf.audience);
+      totalAmount += priceToPay;
     }
-    result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
+    result += String.format("Amount owed is %s\n", frmt.format(totalAmount));
     result += String.format("You earned %s credits\n", volumeCredits);
     return result;
   }
